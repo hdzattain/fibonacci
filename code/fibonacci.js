2bc4cc4c2ff4ba5da33468a058bf318c30a7d4f9
hdzattain <58393005+hdzattain@users.noreply.github.com>
2020-06-26 19:53:07 +0800

    Create fibonacci.js

---
+++ code/fibonacci.js
@@ -0,0 +1,15 @@
+var fibonacci = (function () {
+  var memory = {}
+  return function(n) {
+    if(n==0 || n == 1) {
+      return n
+    }
+    if(memory[n-2] === undefined) {
+      memory[n-2] = fibonacci(n-2)
+    }
+    if(memory[n-1] === undefined) {
+      memory[n-1] = fibonacci(n-1)
+    }
+    return memory[n] = memory[n-1] + memory[n-2]
+  }
+})()
